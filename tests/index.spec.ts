86e3112246d5e0067a71de2ec682318489e9e6e8
Stijn de Vries <s.devries@gen25.com>
2022-06-02 12:48:50 +0200

    Added resource request with the abbility to specify the extra fields you want to fetch.

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,5 @@
 import Booker25 from '../src/index'
 
 test('Booker25 has a version number', () => {
-  const booker25 = new Booker25()
-  expect(booker25.version).toBe('0.0.1')
+  expect(Booker25.version).toBe('0.0.1')
 })
