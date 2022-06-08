5e522c74cf1531706aa9568b081300fa2a991f34
Stijn de Vries <s.devries@gen25.com>
2022-06-08 16:17:56 +0200

    Added setResource to the reservation

--- src/reservation.ts
+++ src/reservation.ts
@@ -1,9 +1,15 @@
+import Resource from './resource'
 
 export default class Reservation {
   private readonly customProperties: Map<string, any>
+  private resource: Resource | null = null
 
   constructor () {
     this.customProperties = new Map()
+  }
+
+  public setResource (resource: Resource): void {
+    this.resource = resource
   }
 
   public setCustomProperty (propertyName: string, value: any): void {
@@ -19,6 +25,9 @@ export default class Reservation {
     this.customProperties.forEach((value, fieldName) => {
       reservationData[fieldName] = value
     })
+    if (this.resource !== null) {
+      reservationData.B25__Resource__c = this.resource.id
+    }
     return {
       reservation: reservationData
     }
