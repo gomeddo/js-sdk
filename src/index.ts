9a887343e19905afbf8d3b6dd65c39f2bbc09c6d
Stijn de Vries <s.devries@gen25.com>
2022-07-05 11:40:36 +0200

    Updated price calculations to include the landing page price fields

--- src/index.ts
+++ src/index.ts
@@ -30,28 +30,35 @@ class Booker25 {
     return outputReservation
   }
 
-  // TODO what should we do with the new pricing fields in the extension package.
-  // If we calculate them here and you don't have it installed the save will fail.
   public async calculatePrice (reservation: Reservation): Promise<Reservation> {
     const updatedPriceCalculationData = await this.api.calculatePrice(JSON.stringify(reservation.getPriceCalculationData()))
     Object.entries(updatedPriceCalculationData.reservation).forEach(([fieldName, value]) => reservation.setCustomProperty(fieldName, value))
     updatedPriceCalculationData.serviceReservations.forEach((serviceReservationData: any, index: number) => {
       Object.entries(serviceReservationData).forEach(([fieldName, value]) => reservation.serviceReservations[index].setCustomProperty(fieldName, value))
     })
-    const totalServiceCost = reservation.serviceReservations.reduce((serviceCosts, serviceReservation) => {
+    const serviceCosts = reservation.serviceReservations.reduce((serviceCosts, serviceReservation) => {
       const quantity = serviceReservation.getCustomProperty('B25__Quantity__c') ?? 0
       const unitPrice = serviceReservation.getCustomProperty('B25__Unit_Price__c') ?? 0
+      const vatRate = serviceReservation.getCustomProperty('B25LP__VAT_Rate__c') ?? 0
       serviceReservation.quantity = quantity
       serviceReservation.unitPrice = unitPrice
       const subtotal = quantity * unitPrice
+      const subtotalIncl = subtotal + (subtotal * vatRate)
       serviceReservation.setCustomProperty('B25__Subtotal__c', subtotal)
-      return serviceCosts + (quantity * unitPrice)
-    }, 0)
-
-    reservation.setCustomProperty('B25__Service_Costs__c', totalServiceCost)
+      serviceReservation.setCustomProperty('B25LP__Subtotal_Incl__c', subtotalIncl)
+      serviceCosts.serviceCosts = serviceCosts.serviceCosts + subtotal
+      serviceCosts.serviceCostsIncl = serviceCosts.serviceCostsIncl + subtotalIncl
+      return serviceCosts
+    }, { serviceCosts: 0, serviceCostsIncl: 0 })
+    reservation.setCustomProperty('B25__Service_Costs__c', serviceCosts.serviceCosts)
+    reservation.setCustomProperty('B25LP__Service_Costs_Incl__c', serviceCosts.serviceCostsIncl)
     const priceFieldValue = reservation.getCustomProperty('B25__Price__c')
     const subtotalValue = (reservation.getCustomProperty('B25__Subtotal__c') ?? 0) as number
-    reservation.setCustomProperty('Total_Price__c', priceFieldValue ?? (subtotalValue + totalServiceCost))
+    const vatRate = (reservation.getCustomProperty('B25LP__VAT_Rate__c') ?? 0) as number
+    const subtotalIncl = subtotalValue + (subtotalValue * vatRate)
+    reservation.setCustomProperty('B25LP__Subtotal_Incl__c', subtotalIncl)
+    reservation.setCustomProperty('B25LP__Total_Incl__c', serviceCosts.serviceCostsIncl + subtotalIncl)
+    reservation.setCustomProperty('B25__Total_Price__c', priceFieldValue ?? (subtotalValue + serviceCosts.serviceCosts))
     return reservation
   }
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -55,7 +55,42 @@ test('You can recalculate the price of the reservation through it', async () => 
   reservation.addService(service, 12)
   const result = await (new Booker25(Enviroment.PRODUCTION)).calculatePrice(reservation)
   expect(result.getCustomProperty('B25__Subtotal__c')).toStrictEqual(150)
+  expect(result.getCustomProperty('B25LP__Subtotal_Incl__c')).toStrictEqual(150)
   expect(result.getCustomProperty('B25__Service_Costs__c')).toStrictEqual(276)
-  expect(result.getCustomProperty('Total_Price__c')).toStrictEqual(150 + 276)
+  expect(result.getCustomProperty('B25LP__Service_Costs_Incl__c')).toStrictEqual(276)
+  expect(result.getCustomProperty('B25__Total_Price__c')).toStrictEqual(150 + 276)
+  expect(result.getCustomProperty('B25LP__Total_Incl__c')).toStrictEqual(150 + 276)
   expect(result.serviceReservations[0].getCustomProperty('B25__Subtotal__c')).toStrictEqual(276)
+  expect(result.serviceReservations[0].getCustomProperty('B25LP__Subtotal_Incl__c')).toStrictEqual(276)
+})
+
+test('You can recalculate the price of the reservation through it with VAT rates specified', async () => {
+  fetchMock.once(JSON.stringify({
+    reservation: {
+      B25__Subtotal__c: 150,
+      B25LP__VAT_Rate__c: 0.2
+    },
+    serviceReservations: [{
+      B25__Quantity__c: 12,
+      B25__Unit_Price__c: 23,
+      B25LP__VAT_Rate__c: 0.1
+    }],
+    serviceCosts: 276
+  }))
+  const reservation = new Reservation()
+  reservation.setCustomProperty('B25__Quantity__c', 10)
+  reservation.setCustomProperty('B25__Base_Price__c', 15)
+  const service = new Service({
+    B25__Price__c: 23
+  }, [])
+  reservation.addService(service, 12)
+  const result = await (new Booker25(Enviroment.PRODUCTION)).calculatePrice(reservation)
+  expect(result.getCustomProperty('B25__Subtotal__c')).toStrictEqual(150)
+  expect(result.getCustomProperty('B25LP__Subtotal_Incl__c')).toStrictEqual(150 + (150 * 0.2))
+  expect(result.getCustomProperty('B25__Service_Costs__c')).toStrictEqual(276)
+  expect(result.getCustomProperty('B25LP__Service_Costs_Incl__c')).toStrictEqual(276 + (276 * 0.1))
+  expect(result.getCustomProperty('B25__Total_Price__c')).toStrictEqual(150 + 276)
+  expect(result.getCustomProperty('B25LP__Total_Incl__c')).toStrictEqual(150 + (150 * 0.2) + 276 + (276 * 0.1))
+  expect(result.serviceReservations[0].getCustomProperty('B25__Subtotal__c')).toStrictEqual(276)
+  expect(result.serviceReservations[0].getCustomProperty('B25LP__Subtotal_Incl__c')).toStrictEqual(276 + (276 * 0.1))
 })
