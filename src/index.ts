83ed766c90dafca3d0990f699dcffadd01431b60
John deGuise <j.deguise@gomeddo.com>
2025-07-28 14:07:15 +0200

    version bump

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ enum Environment {
  * GoMeddo object allows for interaction with GoMeddo
  */
 class GoMeddo {
-  static version: string = '0.0.8'
+  static version: string = '0.0.12'
   private readonly environment: Environment
   private readonly api: GoMeddoAPI
 
