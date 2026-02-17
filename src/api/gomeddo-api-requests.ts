eac63b57dce765c1c6030649fbf695f9eb39be42
Maxime Pagnoulle <mpagnoulle@gmail.com>
2026-02-17 16:04:06 +0100

    GD-6037 Add setRelatedRecords onto Blueprint Timeslots endpoint

--- src/api/gomeddo-api-requests.ts
+++ src/api/gomeddo-api-requests.ts
@@ -233,10 +233,10 @@ export default class GoMeddoAPI {
   }
 
   public async getBlueprintTimeslots (
-    blueprintName: string, duration: number, interval: number, timeslotRange: DateRange, mdaRange: DateRange, fixedSlots: ApexTimeSlot[] | null, prototype: Partial<SFReservation>
+    blueprintName: string, duration: number, interval: number, timeslotRange: DateRange, mdaRange: DateRange, fixedSlots: ApexTimeSlot[] | null, prototype: Partial<SFReservation>, relatedRecords: Record<string, Array<Partial<CustomSFSObject>>> | null
   ): Promise<ReservationCollectionTimeSlot[]> {
     const url = new URL('B25/v1/blueprints/timeSlots', this.baseUrl)
-    const blueprintTimeslotGenerationBody = new BlueprintTimeslotGenerationBody(blueprintName, duration, interval, timeslotRange, mdaRange, fixedSlots, prototype)
+    const blueprintTimeslotGenerationBody = new BlueprintTimeslotGenerationBody(blueprintName, duration, interval, timeslotRange, mdaRange, fixedSlots, prototype, relatedRecords)
     const response = await fetch(url.href, {
       method: 'POST',
       body: JSON.stringify(blueprintTimeslotGenerationBody),
