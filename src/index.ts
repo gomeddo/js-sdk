552408a3e8b069ed3396ab807d4b2282a09f343c
Maxime Pagnoulle <mpagnoulle@gmail.com>
2026-03-26 13:19:42 +0100

    release 0.0.26

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ enum Environment {
  * GoMeddo object allows for interaction with GoMeddo
  */
 class GoMeddo {
-  static version: string = '0.0.25'
+  static version: string = '0.0.26'
   private readonly environment: Environment
   private readonly api: GoMeddoAPI
 
